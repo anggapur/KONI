e8e4fc03e389aa23344a3eb09380126665c22d53
suryaadhisam <suryaadhiixe@gmail.com>
2018-12-03 10:45:17 +0800

    tes

--- app/Kabupaten.php
+++ app/Kabupaten.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kabupaten extends Model
 {
+	// tes
     protected $table = "kabupaten";
 	protected $primaryKey = 'id_kabupaten';
 	public $timestamps = false;
